4aab3ab2072017cc452197bf91651d3f9889307d
ASak1104 <khw56184@gmail.com>
2023-02-01 23:42:38 +0900

    add leetcode42 two array solution

---
+++ leetcode/Trapping Rain Water3.kt
@@ -0,0 +1,32 @@
+package leetcode
+
+import kotlin.math.max
+import kotlin.math.min
+
+class TrappingRainWater3 {
+    // two array
+    fun trap(height: IntArray): Int {
+        var area = 0
+        val leftMax = IntArray(height.size) { height.first()}
+        val rightMax = IntArray(height.size) { height.last() }
+
+        for (i in 1 until height.size) {
+            leftMax[i] = max(leftMax[i - 1], height[i])
+        }
+        for (i in height.lastIndex - 1 downTo 0) {
+            rightMax[i] = max(rightMax[i + 1], height[i])
+        }
+        for ((i, h) in height.withIndex()) {
+            area += min(leftMax[i], rightMax[i]) - h
+        }
+        return area
+    }
+}
+
+fun main() {
+    val sol = TrappingRainWater3()
+    val answer = sol.trap(
+        intArrayOf(0,1,0,2,1,0,1,3,2,1,2,1),
+    )
+    println(answer)
+}
