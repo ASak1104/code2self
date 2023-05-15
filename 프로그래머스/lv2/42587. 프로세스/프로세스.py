2d70a304beaa6bdf5f41aa152cd0c8003cf9190f
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-05-15 13:07:34 +0900

    [level 2] Title: 프로세스, Time: 0.36 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+def solution(priorities, location):
+    que = deque([(i, priorities[i]) for i in range(len(priorities))])
+    cnt = 1
+    while len(que):
+        pMax = max(que, key=lambda x:x[1])[1]
+        e = que.popleft()
+        if e[1] == pMax:
+            if e[0] == location:
+                return cnt
+            cnt += 1
+        else:
+            que.append(e)
+    return -1
