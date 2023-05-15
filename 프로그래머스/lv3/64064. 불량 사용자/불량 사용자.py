a22fbc6fca759055c85641d0af98e7a74d417f16
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-05-15 11:59:27 +0900

    [level 3] Title: 불량 사용자, Time: 4.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,10 @@
+import re
+from itertools import permutations as pm
+
+def solution(user_id, banned_id):
+    match_set = set()
+    bid_list = [bid.replace('*', '.') for bid in banned_id]
+    for uid_list in pm(user_id, len(banned_id)):
+        if all(re.match(bid + '$', uid) for uid, bid in zip(uid_list, bid_list)):
+            match_set.add(tuple(sorted(uid_list)))
+    return len(match_set)
