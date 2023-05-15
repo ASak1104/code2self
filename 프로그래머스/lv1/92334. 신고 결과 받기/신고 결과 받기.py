42e474adf62996ee50d44ad70dccedcd6b73d677
ASak1104 <khw56184@gmail.com>
2023-05-15 16:28:28 +0900

    <refactor> : separate solution file

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -10,20 +10,3 @@ def solution(id_list, report, k):
             answer[id_list.index(r.split()[0])] += 1
 
     return answer
-
-# def solution(id_list, report, k):
-#     counts = {iid: 0 for iid in id_list}
-#     reporters = {iid: set() for iid in id_list}
-    
-#     for arr in report:
-#         ifrom, ito = arr.split()
-#         if ifrom not in reporters[ito]:
-#             counts[ito] += 1
-#             reporters[ito].add(ifrom)
-    
-#     res = {iid: 0 for iid in id_list}
-#     for iid, cnts in counts.items():
-#         if cnts >= k:
-#             for ifrom in reporters[iid]:
-#                 res[ifrom] += 1
-#     return list(res.values())
