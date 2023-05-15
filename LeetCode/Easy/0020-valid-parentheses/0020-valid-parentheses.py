72524fcd853598fd9dc9a2b1e4e63b1a4694b2ad
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-05-15 17:23:00 +0900

    Time: 44 ms (22.15%), Space: 16.4 MB (9.9%) - LeetHub

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    right_bracket_map: Dict = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for bracket in s:
+            if bracket in self.right_bracket_map:
+                if not stack or stack.pop() != self.right_bracket_map[bracket]:
+                    return False
+            else:
+                stack.append(bracket)
+        return len(stack) == 0
