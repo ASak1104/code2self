56c14114f8ecac4d7959248980bce2d85a99602b
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-05-15 15:07:38 +0900

    [level 2] Title: 괄호 변환, Time: 0.44 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,32 @@
+def solution(p):
+    if isValidBracket(p):
+        return p
+    u, v = divideBracket(p)
+    if isValidBracket(u):
+        return u + solution(v)
+    temp = ['(' if c == ')' else ')' for c in u[1:-1]]
+    return f'({solution(v)})' + ''.join(temp)
+
+
+def isValidBracket(p):
+    stack = []
+    for c in p:
+        if c == ')':
+            if not stack:
+                return False
+            stack.pop()
+        else:
+            stack.append(c)
+    return True
+
+
+def divideBracket(p):
+    count = 0
+    for i, c in enumerate(p):
+        if c == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return p[:i + 1], p[i + 1:]
+    return p
