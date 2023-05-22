d009e109eda2c98f497f5c7a08e31e546615e6e9
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-05-23 02:38:40 +0900

    [Platinum IV] Title: 북서풍, Time: 2496 ms, Memory: 218460 KB -BaekjoonHub

--- 백준/Platinum/5419. 북서풍/북서풍.py
+++ 백준/Platinum/5419. 북서풍/북서풍.py
@@ -3,30 +3,41 @@ import sys
 readline = sys.stdin.readline
 
 
-def update(tree, node, start, end, y):
-    if y < start or y > end:
-        return
-    tree[node] += 1
+def build_tree(n):
+    capacity = 1
+    while capacity < n:
+        capacity = capacity << 1
+    ret = [0] * (capacity << 1)
+    ret[0] = capacity  # index offset
+    return ret
+
+
+def update(tree, y):
+    node = tree[0] + y
+    while node > 0:
+        tree[node] += 1
+        node = node >> 1
+
+
+def query(tree, qs, qe):
+    start = tree[0] + qs
+    end = tree[0] + qe
+    ret = 0
+    while start < end:
+        if start % 2 == 1:
+            ret += tree[start]
+            start += 1
+        if end % 2 == 0:
+            ret += tree[end]
+            end -= 1
+        start = start >> 1
+        end = end >> 1
     if start == end:
-        return
-    child = (node << 1) + 1
-    mid = (start + end) >> 1
-    if y <= mid:
-        update(tree, child, start, mid, y)
-    else:
-        update(tree, child + 1, mid + 1, end, y)
+        ret += tree[start]
+    return ret
 
 
-def query(tree, node, start, end, qs, qe):
-    if end < qs or start > qe:
-        return 0
-    if qs <= start and end <= qe:
-        return tree[node]
-    child = (node << 1) + 1
-    mid = (start + end) >> 1
-    return query(tree, child, start, mid, qs, qe) + query(tree, child + 1, mid + 1, end, qs, qe)
-
-
+results = []
 for _ in range(int(readline())):
     n = int(readline())
     islands = [list(map(int, readline().split())) for _ in range(n)]
@@ -43,8 +54,10 @@ for _ in range(int(readline())):
     islands.sort(key=lambda x: x[0])
 
     answer = 0
-    tree = [0] * ((count + 1) << 2)
+    tree = build_tree(count + 1)
     for _, y in islands:
-        answer += query(tree, 0, 0, count, y, count)
-        update(tree, 0, 0, count, y)
-    print(answer)
+        answer += query(tree, y, count)
+        update(tree, y)
+    results.append(str(answer))
+
+print('\n'.join(results))
