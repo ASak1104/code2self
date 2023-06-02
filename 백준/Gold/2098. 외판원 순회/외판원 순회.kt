b77938616ba09cb16cb92a25072d15d8b024e3f7
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-06-02 13:19:48 +0900

    [Gold I] Title: 외판원 순회, Time: 188 ms, Memory: 24004 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.kt
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.kt
@@ -2,59 +2,43 @@ import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    val costs = Array(n) { IntArray(n) }
-
-    repeat(n) { r ->
-        val line = StringTokenizer(readLine())
-        for (c in 0 until n) {
-            costs[r][c] = line.nextToken().toInt()
-        }
+    val costs = Array(n) {
+        StringTokenizer(readLine()).run { IntArray(n) { nextToken().toInt() } }
     }
-
     val tsp = TSP(n, costs)
-    tsp.tsp()
-    print(tsp.getMin())
+    print(tsp.travel(1, 0))
+    close()
 }
 
 class TSP(val n: Int, val costs: Array<IntArray>) {
-    val memo = Array(1 shl n) { IntArray(n) { Int.MAX_VALUE } }
+    val MAX_VALUE = 16_000_001
+    val memo = Array(1 shl n) { IntArray(n) }
 
-    fun tsp() {
-        travel(1, 0, 0)
-        val last = memo.lastIndex
-
-        for (i in 0 until n) {
-            if (costs[i][0] == 0) {
-                memo[last][i] = Int.MAX_VALUE
-            }
-            else if (memo[last][i] != Int.MAX_VALUE) {
-                memo[last][i] += costs[i][0]
-            }
-        }
-    }
-
-    fun travel(mask: Int, u: Int, cost: Int) {
+    fun travel(mask: Int, u: Int): Int {
         if (mask == memo.lastIndex) {
-            if (cost < memo[mask][u]) {
-                memo[mask][u] = cost
-            }
-            return
+            return if (costs[u][0] == 0)
+                MAX_VALUE
+            else
+                costs[u][0]
         }
+        if (memo[mask][u] > 0) {
+            return memo[mask][u]
+        }
+        var minCost = MAX_VALUE
+
         for (v in 0 until n) {
             val bit = 1 shl v
 
             if (mask and bit == bit || costs[u][v] == 0) {
                 continue
             }
-            val nextMask = mask or bit
-            val nextCost = cost + costs[u][v]
+            val nextCost = costs[u][v] + travel(mask or bit, v)
 
-            if (nextCost < memo[nextMask][v]) {
-                memo[nextMask][v] = nextCost
-                travel(nextMask, v, nextCost)
+            if (nextCost < minCost) {
+                minCost = nextCost
             }
         }
+        memo[mask][u] = minCost
+        return minCost
     }
-
-    fun getMin() = memo[memo.lastIndex].min()
 }
