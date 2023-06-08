d4cf68f59cd985bda0b51f04ab0e72052fbcc58c
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-06-08 19:41:48 +0900

    [Gold IV] Title: 스도쿠, Time: 2632 ms, Memory: 305372 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.kt
@@ -0,0 +1,56 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val sudoku = Sudoku(Array(9) { readLine().map { it - '0' }.toIntArray() })
+    close()
+    
+    sudoku.solve(0, 0)
+    
+    val bw = System.out.bufferedWriter()
+    sudoku.board.forEach {
+        bw.append(it.joinToString(""))
+        bw.newLine()
+    }
+    bw.flush()
+    bw.close()
+}
+
+class Sudoku(val board: Array<IntArray>) {
+    
+    fun solve(sr: Int, sc: Int): Boolean {
+        for (c in sc until 9) {
+            if (board[sr][c] > 0) continue
+
+            if (!nextStep(sr, c)) return false
+        }
+        for (r in sr + 1 until 9) {
+            for (c in board.indices) {
+                if (board[r][c] > 0) continue
+
+                if (!nextStep(r, c)) return false
+            }
+        }
+        return true
+    }
+
+    fun nextStep(r: Int, c: Int): Boolean {
+        board[r][c] = getMin(r, c, 1)
+        if (board[r][c] == 0) return false
+
+        while (!solve(r, c + 1)) {
+            board[r][c] = getMin(r, c, board[r][c] + 1)
+            if (board[r][c] == 0) return false
+        }
+        return true
+    }
+
+    fun getMin(sr: Int, sc: Int, start: Int): Int {
+        val set = (start..9).toMutableSet()
+        repeat(9) { set -= board[it][sc] }
+        repeat(9) { set -= board[sr][it] }
+        repeat(3) { ir ->
+            val r = (sr / 3) * 3
+            val c = (sc / 3) * 3
+            repeat(3) { ic -> set -= board[r + ir][c + ic] }
+        }
+        return set.minOrNull() ?: 0
+    }
+}
