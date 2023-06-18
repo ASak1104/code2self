905d370fef783d7e921fce5d9d771aa78886949f
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-06-18 09:51:07 +0900

    [Silver I] Title: 절댓값 힙, Time: 388 ms, Memory: 55216 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.kt
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.kt
@@ -5,12 +5,7 @@ fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
 
     val bw = System.out.bufferedWriter()
-    val pq = PriorityQueue<Int>(10000) { a, b ->
-        when (abs(a)) {
-            abs(b) -> a - b
-            else -> abs(a) - abs(b)
-        }
-    }
+    val pq = PriorityQueue(100000, compareBy<Int>(::abs).thenBy { it })
 
     repeat(n) {
         when (val x = readLine().toInt()) {
