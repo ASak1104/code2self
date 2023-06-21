6eb66949ceebe2090fe2a7746c5aa1fa084cd57f
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-06-22 00:34:34 +0900

    [Gold III] Title: 파티, Time: 220 ms, Memory: 18780 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.kt
+++ 백준/Gold/1238. 파티/파티.kt
@@ -4,7 +4,8 @@ fun main() = with(System.`in`.bufferedReader()) {
     val (n, m, x) = StringTokenizer(readLine()).run {
         Triple(nextToken().toInt(), nextToken().toInt(), nextToken().toInt() - 1)
     }
-    val dist = Array(n) { IntArray(n) { Int.MAX_VALUE } }
+    val edges = Array(n) { ArrayList<Edge>() }
+    val reverseEdge = Array(n) { ArrayList<Edge>() }
 
     repeat(m) {
         with(StringTokenizer(readLine())) {
@@ -12,28 +13,17 @@ fun main() = with(System.`in`.bufferedReader()) {
             val v = nextToken().toInt() - 1
             val c = nextToken().toInt()
 
-            dist[u][v] = c
-        }
-
-        if (it < n) dist[it][it] = 0
-    }
-
-    for (k in dist.indices) {
-        for (u in dist.indices) {
-            for (v in dist.indices) {
-                if (dist[u][k] == Int.MAX_VALUE || dist[k][v] == Int.MAX_VALUE) continue
-
-                val d = dist[u][k] + dist[k][v]
-
-                if (dist[u][v] > d) dist[u][v] = d
-            }
+            edges[u].add(Edge(v, c))
+            reverseEdge[v].add(Edge(u, c))
         }
     }
 
+    val dist = dijkstra(edges, x)
+    val reverseDist = dijkstra(reverseEdge, x)
     var res = 0
 
     for (u in dist.indices) {
-        val t = dist[u][x] + dist[x][u]
+        val t = dist[u] + reverseDist[u]
 
         if (res < t) res = t
     }
@@ -45,3 +35,28 @@ fun main() = with(System.`in`.bufferedReader()) {
     }
     close()
 }
+
+fun dijkstra(edges: Array<ArrayList<Edge>>, x: Int): IntArray {
+    val dist = IntArray(edges.size) { Int.MAX_VALUE }
+    val pq = PriorityQueue(dist.size, compareBy(Edge::c))
+
+    dist[x] = 0
+    pq.add(Edge(x, 0))
+
+    while (pq.isNotEmpty()) {
+        val (u, d) = pq.poll()
+
+        for ((v, c) in edges[u]) {
+            val dc = d + c
+
+            if (dist[v] > dc) {
+                dist[v] = dc
+                pq.add(Edge(v, dc))
+            }
+        }
+    }
+
+    return dist
+}
+
+data class Edge(val v: Int, val c: Int)
