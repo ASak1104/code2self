8ab058590668ce6f5ef40c84312c59b67a30b4c9
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-06-22 15:50:09 +0900

    [Gold V] Title: 동전 1, Time: 140 ms, Memory: 13684 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.kt
@@ -0,0 +1,27 @@
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    val readInt = {
+        nextToken()
+        nval.toInt()
+    }
+    val n = readInt()
+    val k = readInt()
+    val coins = MutableList(n) { readInt() }
+    val memo = IntArray(k + 1)
+
+    memo[0] = 1
+
+    for (c in coins) {
+        for (s in 0 until k) {
+            if (s + c > k) continue
+
+            memo[s + c] += memo[s]
+        }
+    }
+
+    with(System.out.bufferedWriter()) {
+        append("${memo[k]}").flush()
+        close()
+    }
+}
