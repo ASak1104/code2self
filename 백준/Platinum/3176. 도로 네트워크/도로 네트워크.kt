28548af34cbdbbc6bbbef1622379fa54fff040be
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-03 19:55:45 +0900

    [Platinum IV] Title: 도로 네트워크, Time: 1104 ms, Memory: 140500 KB -BaekjoonHub

--- 백준/Platinum/3176. 도로 네트워크/도로 네트워크.kt
+++ 백준/Platinum/3176. 도로 네트워크/도로 네트워크.kt
@@ -9,6 +9,7 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
         nextToken()
         nval.toInt()
     }
+
     val n = readInt()
     val network = Network(n)
 
@@ -18,20 +19,18 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
 
     network.build()
 
-    val bw = System.out.bufferedWriter()
+    with(System.out.bufferedWriter()) {
+        repeat(readInt()) {
+            append(network.query(readInt(), readInt()))
+        }
 
-    repeat(readInt()) {
-        val (min, max) = network.query(readInt(), readInt())
-
-        bw.append("$min $max\n")
+        flush()
+        close()
     }
-
-    bw.flush()
-    bw.close()
 }
 
 class Network(val n: Int) {
-    val edges = Array(n + 1) { ArrayList<Pair<Int, Int>>() }
+    val edges = Array(n + 1) { ArrayList<Edge>() }
     val depths = IntArray(n + 1)
     val height = log2(n.toDouble()).toInt()
     val ancestors = Array(height + 1) { IntArray(n + 1) }
@@ -39,75 +38,65 @@ class Network(val n: Int) {
     val maxs = Array(height + 1) { Array(n + 1) { MIN } }
 
     fun addEdge(u: Int, v: Int, w: Int) {
-        edges[u] += v to w
-        edges[v] += u to w
+        edges[u] += Edge(v, w)
+        edges[v] += Edge(u, w)
     }
 
     fun build() {
-        val nodes = ArrayList<Int>(n)
+        depths[0] = -1
 
-        fun dfs(u: Int, p: Int) {
-            nodes.add(u)
-            depths[u] = depths[p] + 1
-            ancestors[0][u] = p
+        travel(1, 0)
 
-            for (i in 1..height) {
+        for (i in 1..height) {
+            for (u in 2..n) {
+                if (ancestors[i - 1][u] == 0) continue
+
                 val au = ancestors[i - 1][u]
 
                 ancestors[i][u] = ancestors[i - 1][au]
-            }
-
-            for ((v, w) in edges[u]) {
-                when {
-                    v != p -> dfs(v, u)
-                    else -> {
-                        mins[0][u] = w
-                        maxs[0][u] = w
-                    }
-                }
-            }
-        }
-
-        depths[0] = -1
-        dfs(1, 0)
-
-        for (u in nodes) {
-            for (i in 1..height) {
-                if (ancestors[i][u] == 0) break
-
-                val au = ancestors[i - 1][u]
-
                 mins[i][u] = minOf(mins[i - 1][u], mins[i - 1][au])
                 maxs[i][u] = maxOf(maxs[i - 1][u], maxs[i - 1][au])
             }
         }
     }
 
-    fun query(qu: Int, qv: Int): Pair<Int, Int> {
+    fun travel(u: Int, p: Int) {
+        depths[u] = depths[p] + 1
+        ancestors[0][u] = p
+
+        for (edge in edges[u]) {
+            if (edge.v == p) {
+                mins[0][u] = edge.w
+                maxs[0][u] = edge.w
+                continue
+            }
+
+            travel(edge.v, u)
+        }
+    }
+
+    fun query(qu: Int, qv: Int): String {
         if (depths[qu] < depths[qv]) return query(qv, qu)
 
-        var u = qu
         var min = MAX
         var max = MIN
-
-        while (depths[u] != depths[qv]) {
-            var i = height
-
-            while (i > 0 && depths[ancestors[i][u]] < depths[qv]) i--
-
-            min = minOf(min, mins[i][u])
-            max = maxOf(max, maxs[i][u])
-            u = ancestors[i][u]
-        }
-
-        if (u == qv) return min to max
-
+        var u = qu
         var v = qv
 
-        while (u != v) {
-            var i = height
+        for (i in height downTo 0) {
+            if (depths[u] == depths[v]) break
 
-            while (i > 0 && ancestors[i][u] == ancestors[i][v]) i--
+            if (depths[ancestors[i][u]] >= depths[v]) {
+                min = minOf(min, mins[i][u])
+                max = maxOf(max, maxs[i][u])
+                u = ancestors[i][u]
+            }
+        }
+
+        if (u == v) return "$min $max\n"
+
+        for (i in height downTo 0) {
+            if (ancestors[i][u] == ancestors[i][v]) continue
 
             min = minOf(min, mins[i][u], mins[i][v])
             max = maxOf(max, maxs[i][u], maxs[i][v])
@@ -115,6 +104,11 @@ class Network(val n: Int) {
             v = ancestors[i][v]
         }
 
-        return min to max
+        min = minOf(min, mins[0][u], mins[0][v])
+        max = maxOf(max, maxs[0][u], maxs[0][v])
+
+        return "$min $max\n"
     }
 }
+
+data class Edge(val v: Int, val w: Int)
