5c28a69b4ebc7b19c2f5a64c142a7b31279fa141
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-11 17:03:10 +0900

    [Gold V] Title: 수 나누기 게임, Time: 1388 ms, Memory: 218196 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.kt
@@ -0,0 +1,32 @@
+import java.io.StreamTokenizer
+
+private val st = StreamTokenizer(System.`in`.bufferedReader())
+
+private fun readInt(): Int {
+    st.nextToken()
+    return st.nval.toInt()
+}
+
+fun main() {
+    val n = readInt()
+    val players = IntArray(n) { readInt() }
+    val playerSet = players.toSortedSet()
+
+    val maxValue = playerSet.last()
+    val points = IntArray(maxValue + 1)
+
+    for (user in players) {
+        for (k in user * 2..maxValue step user) {
+            if (k in playerSet) {
+                points[user]++
+                points[k] -= 1
+            }
+        }
+    }
+
+    with(System.out.bufferedWriter()) {
+        players.forEach { append("${points[it]} ") }
+        flush()
+        close()
+    }
+}
