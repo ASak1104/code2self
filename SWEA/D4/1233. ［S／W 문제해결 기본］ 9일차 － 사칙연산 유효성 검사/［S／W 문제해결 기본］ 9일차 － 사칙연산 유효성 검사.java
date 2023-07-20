a29fb214a5f669b99b84d7f3dd8438f5688d15e6
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-21 03:41:03 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 265 ms, Memory: 53,140 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -2,33 +2,31 @@ import java.io.*;
 import java.util.Scanner;
 
 class Solution {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static Scanner sc;
-
-    static void readLine() throws IOException {
-        sc = new Scanner(br.readLine());
-    }
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         for (int tc = 1; tc <= 10; tc++) {
-            readLine();
-
-            int n = sc.nextInt();
+            int n = Integer.parseInt(br.readLine());
             boolean calculable = true;
 
             while (n-- > 0) {
-                readLine();
-
-                if (calculable) {
-                    sc.next();
-
-                    boolean isNumber = sc.hasNextInt();
-
-                    sc.next();
-                    calculable = isNumber ^ sc.hasNext();
+                if (!calculable) {
+                    br.readLine();
+                    continue;
                 }
+
+                Scanner sc = new Scanner(br.readLine());
+
+                sc.next();
+
+                boolean isNumber = sc.hasNextInt();
+
+                sc.next();
+                
+                calculable = isNumber ^ sc.hasNext();
+                
+                sc.close();
             }
 
             bw.append(String.format("#%d %d\n", tc, calculable ? 1 : 0));
