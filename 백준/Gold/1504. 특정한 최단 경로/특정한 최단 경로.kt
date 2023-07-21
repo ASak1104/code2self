5854f6905a9563ae5303875f96446ce6492c6b42
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-22 04:36:02 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 676 ms, Memory: 41008 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.kt
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.kt
@@ -1,7 +1,11 @@
 import java.io.StreamTokenizer
+import java.util.*
 
 val INF = 1e8.toInt()
 val st = StreamTokenizer(System.`in`.bufferedReader())
+
+lateinit var edges: Array<ArrayList<Node>>
+var n = 0
 
 fun readInt(): Int = with(st) {
     nextToken()
@@ -9,43 +13,26 @@ fun readInt(): Int = with(st) {
 }
 
 fun main() {
-    val n = readInt()
+    n = readInt()
     val m = readInt()
 
-    val dists = Array(n) { IntArray(n) { INF } }
-
-    repeat(n) { dists[it][it] = 0 }
+    edges = Array(n) { ArrayList<Node>() }
 
     repeat(m) {
         val u = readInt() - 1
         val v = readInt() - 1
         val w = readInt()
 
-        if (dists[u][v] > w) {
-            dists[u][v] = w
-            dists[v][u] = w
-        }
-
+        edges[u] += Node(v, w)
+        edges[v] += Node(u, w)
     }
 
     val v1 = readInt() - 1
     val v2 = readInt() - 1
 
-    for (k in dists.indices) {
-        for (u in dists.indices) {
-            for (v in dists.indices) {
-                val dist = dists[u][k] + dists[k][v]
-
-                if (dists[u][v] > dist) {
-                    dists[u][v] = dist
-                }
-            }
-        }
-    }
-
     val res = minOf(
-        dists[0][v1] + dists[v1][v2] + dists[v2][n - 1],
-        dists[0][v2] + dists[v2][v1] + dists[v1][n - 1]
+        dijkstra(0, v1) + dijkstra(v1, v2) + dijkstra(v2, n - 1),
+        dijkstra(0, v2) + dijkstra(v2, v1) + dijkstra(v1, n - 1)
     )
 
     with(System.out.bufferedWriter()) {
@@ -54,3 +41,30 @@ fun main() {
         close()
     }
 }
+
+fun dijkstra(s: Int, e: Int): Int {
+    if (s == e) return 0
+
+    val pq = PriorityQueue(compareBy(Node::weight))
+    val dists = IntArray(n) { INF }
+
+    pq += Node(s, 0)
+    dists[s] = 0
+
+    while (pq.isNotEmpty()) {
+        val u = pq.poll()
+
+        for (v in edges[u.node]) {
+            val w = u.weight + v.weight
+
+            if (dists[v.node] > w) {
+                dists[v.node] = w
+                pq += Node(v.node, w)
+            }
+        }
+    }
+
+    return dists[e]
+}
+
+data class Node(val node: Int, val weight: Int)
