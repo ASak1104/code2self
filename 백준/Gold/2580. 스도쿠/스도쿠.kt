ab4030ba50e34fefd1b1cc042246903cebc6caaf
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-24 18:00:09 +0900

    [Gold IV] Title: 스도쿠, Time: 1852 ms, Memory: 308700 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.kt
+++ 백준/Gold/2580. 스도쿠/스도쿠.kt
@@ -1,21 +1,25 @@
-import java.util.*
+import java.io.StreamTokenizer
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val board = Array(9) {
-        StringTokenizer(readLine()).run { IntArray(9) { nextToken().toInt() } }
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    fun readInt(): Int {
+        nextToken()
+        return nval.toInt()
     }
+
+    val board = Array(9) { IntArray(9) { readInt() } }
     val sudoku = Sudoku(board)
-    close()
 
     sudoku.solve(0, 0)
 
-    val bw = System.out.bufferedWriter()
-    board.forEach {
-        bw.append(it.joinToString(" "))
-        bw.newLine()
+    with(System.out.bufferedWriter()) {
+        board.forEach {
+            it.joinTo(this, " ")
+            newLine()
+        }
+
+        flush()
+        close()
     }
-    bw.flush()
-    bw.close()
 }
 
 class Sudoku(val board: Array<IntArray>) {
@@ -26,6 +30,7 @@ class Sudoku(val board: Array<IntArray>) {
 
             if (!nextStep(sr, c)) return false
         }
+
         for (r in sr + 1 until 9) {
             for (c in board.indices) {
                 if (board[r][c] > 0) continue
@@ -33,29 +38,38 @@ class Sudoku(val board: Array<IntArray>) {
                 if (!nextStep(r, c)) return false
             }
         }
+
         return true
     }
 
     fun nextStep(r: Int, c: Int): Boolean {
         board[r][c] = getMin(r, c, 1)
+
         if (board[r][c] == 0) return false
 
         while (!solve(r, c + 1)) {
             board[r][c] = getMin(r, c, board[r][c] + 1)
+
             if (board[r][c] == 0) return false
         }
+
         return true
     }
 
     fun getMin(sr: Int, sc: Int, start: Int): Int {
         val set = (start..9).toMutableSet()
+
         repeat(9) { set -= board[it][sc] }
+
         repeat(9) { set -= board[sr][it] }
+
         repeat(3) { ir ->
             val r = (sr / 3) * 3
             val c = (sc / 3) * 3
+
             repeat(3) { ic -> set -= board[r + ir][c + ic] }
         }
+
         return set.minOrNull() ?: 0
     }
 }
