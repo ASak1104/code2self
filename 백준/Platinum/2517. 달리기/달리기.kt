939d07ed635919ce72ba66501f25878c016ce17c
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-25 18:03:23 +0900

    [Platinum IV] Title: 달리기, Time: 1320 ms, Memory: 99960 KB -BaekjoonHub

--- 백준/Platinum/2517. 달리기/달리기.kt
+++ 백준/Platinum/2517. 달리기/달리기.kt
@@ -1,44 +1,66 @@
-class SegmentTree(private val n: Int) {
-    private val tree = IntArray(n * 4)
-    private val ranks = IntArray(n)
-    
-    fun getRank(): IntArray = ranks
+import java.io.StreamTokenizer
+
+val st = StreamTokenizer(System.`in`.bufferedReader())
+
+fun readInt() = with(st) {
+    nextToken()
+    nval.toInt()
+}
+
+fun main() {
+    val n = readInt()
+    val athletes = IntArray(n) { readInt() }
+    val sortedPairs = athletes.withIndex().sortedBy { it.value }
+
+    for ((i, p) in sortedPairs.withIndex()) {
+        athletes[p.index] = i
+    }
+
+    val st = SegmentTree(n)
+
+    for (athlete in athletes) {
+        st.update(athlete)
+    }
+
+    val ranks = st.ranks
+
+    for (i in athletes.indices) {
+        athletes[i] = ranks[athletes[i]]
+    }
+
+    with(System.out.bufferedWriter()) {
+		athletes.forEach { append("$it\n") }
+
+        flush()
+        close()
+    }
+}
+
+class SegmentTree(val n: Int) {
+    val tree = IntArray(n * 4)
+    val ranks = IntArray(n)
 
     fun update(node: Int) {
         update(1, 0, n, node)
     }
 
-    private tailrec fun update(i: Int, start: Int, end: Int, node: Int) {
+    tailrec fun update(i: Int, start: Int, end: Int, node: Int) {
         tree[i]++
+
         if (start == end - 1) {
             ranks[node]++
             return
         }
+
         val child = i shl 1
         val mid = (start + end) ushr 1
+
         if (node in start until mid) {
             ranks[node] += tree[child + 1]
-            update(child, start, mid, node)
-        } else {
-            update(child + 1, mid, end, node)
-        }
-    }
-}
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val athletes = IntArray(n) { readLine().toInt() }
-    val sortedPairs = athletes.withIndex().sortedBy { it.value }
-    for ((i, p) in sortedPairs.withIndex()) {
-        athletes[p.index] = i
+            return update(child, start, mid, node)
+        }
+
+        update(child + 1, mid, end, node)
     }
-    val st = SegmentTree(n)
-    for (athlete in athletes) {
-        st.update(athlete)
-    }
-    val ranks = st.getRank()
-    for (i in athletes.indices) {
-        athletes[i] = ranks[athletes[i]]
-    }
-    print(athletes.joinToString("\n"))
 }
