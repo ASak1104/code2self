2418f9401dd2b3792f2d965a3bb9c0908f2d78a1
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-26 13:29:27 +0900

    [Gold II] Title: 보석 도둑, Time: 1344 ms, Memory: 142804 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.kt
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.kt
@@ -1,44 +1,33 @@
 import java.util.*
-import kotlin.collections.ArrayDeque
 
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, k) = StringTokenizer(readLine()).run { nextToken().toInt() to nextToken().toInt() }
     val gems = MutableList(n) {
         StringTokenizer(readLine()).run { nextToken().toInt() to nextToken().toInt() }
     }
-    val bags = ArrayDeque<Int>(k)
-    repeat(k) { bags.add(readLine().toInt()) }
+    gems.sortByDescending { it.second }
+    
+    val bags = TreeMap<Int, Int>()
+    repeat(k) {
+        val bag = readLine().toInt()
+        bags[bag] = (bags[bag] ?: 0) + 1
+    }
     close()
 
-    gems.sortByDescending { it.second }
-    bags.sort()
-
     var res = 0L
-
+    
     for (gem in gems) {
-        if (bags[bags.lastIndex] < gem.first) continue
+        if (bags.lastKey() < gem.first) continue
 
-        bags.removeAt(bisect(bags, gem.first))
         res += gem.second
+        val (key, value) = bags.ceilingEntry(gem.first)
+
+        when (value) {
+            1 -> bags.remove(key)
+            else -> bags[key] = value - 1
+        }
 
         if (bags.size == 0) break
     }
     print(res)
-}
-
-fun bisect(bags: List<Int>, target: Int): Int {
-    var s = 0
-    var e = bags.lastIndex
-
-    while (s < e) {
-        val m = (s + e) ushr 1
-        val v = bags[m]
-
-        when {
-            v == target -> return m
-            v < target -> s = m + 1
-            else -> e = m
-        }
-    }
-    return e
 }
