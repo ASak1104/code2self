41e291a0e9baf4f5345193d585ef5ec429292ccb
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-31 15:09:36 +0900

    [Silver IV] Title: 카드 놓기, Time: 180 ms, Memory: 16716 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+class Main {
+
+    private static final StreamTokenizer sttk = new StreamTokenizer(new InputStreamReader(System.in));
+    private static final int[] cards = new int[10];
+    private static final Set<String> set = new HashSet<>();
+    private static int n;
+    private static int k;
+
+    private static int readInt() throws IOException {
+        sttk.nextToken();
+
+        return (int) sttk.nval;
+    }
+
+    public static void main(String[] args) throws IOException {
+        n = readInt();
+        k = readInt();
+
+        for (int i = 0; i < n; i++) {
+            cards[i] = readInt();
+        }
+
+        travel("", 0, 0);
+
+        System.out.println(set.size());
+    }
+
+    static void travel(String prev, int visit, int count) {
+        if (count == k) {
+            set.add(prev);
+
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int mask = (1 << i);
+
+            if ((visit & mask) == mask) continue;
+
+            travel(prev + cards[i], visit | mask, count + 1);
+        }
+    }
+}
