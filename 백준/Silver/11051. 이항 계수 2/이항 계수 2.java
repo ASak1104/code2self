4bcb05d6c91186e5b1ead32122ae3fb52e9f1693
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-07-31 11:48:37 +0900

    [Silver II] Title: 이항 계수 2, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -3,7 +3,7 @@ import java.io.*;
 class Main {
 
     static StreamTokenizer sttk = new StreamTokenizer(new InputStreamReader(System.in));
-    static int[][] memo = new int[1001][1001];
+    private static final int MOD = 10_007;
 
     static int readInt() throws IOException {
         sttk.nextToken();
@@ -14,24 +14,40 @@ class Main {
         int n = readInt();
         int k = readInt();
 
-        System.out.println(comb(n, k));
+        int c = 1;
+
+        for (int i = 2; i <= k; i++) {
+            c = (c * i) % MOD;
+        }
+
+        int a = combi(n, k);
+        int b = pow(c, MOD - 2);
+
+        System.out.println((a * b) % MOD);
     }
 
-    static int comb(int n, int k) {
-        if (k == 0 || n == k) return 1;
+    // n * n - 1 * ... * n - k + 1
+    static int combi(int n, int k) {
+        int ret = 1;
 
-        if (k == 1 || k == n - 1) {
-            return n;
+        for (int i = n; i > n - k; i--) {
+            ret = (ret * i) % MOD;
         }
 
-        if (memo[n][k] != 0) {
-            return memo[n][k];
+        return ret;
+    }
+
+    static int pow(int x, int y) {
+        int ret = 1;
+
+        while (y > 0) {
+            if ((y & 1) == 1) {
+                ret = (ret * x) % MOD;
+            }
+
+            x = (x * x) % MOD;
+            y >>>= 1;
         }
-
-        int ret = (comb(n - 1, k - 1) + comb(n - 1, k)) % 10_007;
-
-        memo[n][k] = ret;
-        memo[n][n - k] = ret;
 
         return ret;
     }
