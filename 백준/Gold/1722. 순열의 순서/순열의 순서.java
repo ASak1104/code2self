804eb6f0d56a8c36a7fabf221a4114f72ee618a9
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-08-01 10:39:31 +0900

    [Gold V] Title: 순열의 순서, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visit = new boolean[21];
+    static int[] seq = new int[20];
+    static long[] fact = new long[20];
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+
+        fact[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            fact[i] = fact[i - 1] * i;
+        }
+
+        if (k == 1) {
+            findSeq();
+        } else {
+            findOrder();
+        }
+
+        System.out.println(sb);
+    }
+
+    static void findOrder() {
+        for (int i = 0; i < n; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long order = 1;
+        int count = n - 1;
+
+        for (int num : seq) {
+            for (int i = 1; i <= n; i++) {
+                if (visit[i]) continue;
+
+                if (num == i) {
+                    visit[i] = true;
+                    break;
+                }
+
+                order += fact[count];
+            }
+
+            count--;
+        }
+
+        sb.append(order);
+    }
+
+    static void findSeq() {
+        long order = Long.parseLong(st.nextToken());
+        int count = n - 1;
+
+        while (count > 0) {
+            for (int i = 1; i <= n; i++) {
+                if (visit[i]) continue;
+
+                if (order <= fact[count]) {
+                    visit[i] = true;
+                    sb.append(i);
+                    sb.append(' ');
+                    break;
+                }
+
+                order -= fact[count];
+            }
+
+            count--;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i]) {
+                sb.append(i);
+                return;
+            }
+        }
+    }
+}
