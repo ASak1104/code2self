4de1ed06474e75989f8376fb3c05775b0f09ccc5
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-08-01 14:28:26 +0900

    [Gold V] Title: 집합의 표현, Time: 564 ms, Memory: 89512 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,67 @@
+import java.io.*;
+
+class Main {
+
+    static StreamTokenizer sttk = new StreamTokenizer(new InputStreamReader(System.in));
+    static int[] parents;
+    static int[] heights;
+
+    static int readInt() throws IOException {
+        sttk.nextToken();
+        return (int) sttk.nval;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = readInt();
+        int m = readInt();
+        parents = new int[n + 1];
+        heights = new int[n + 1];
+
+        for (int u = 1; u <= n; u++) {
+            parents[u] = u;
+            heights[u] = 1;
+        }
+
+        while (m-- > 0) {
+            int cmd = readInt();
+            int a = readInt();
+            int b = readInt();
+
+            if (cmd == 0) {
+                merge(find(a), find(b));
+                continue;
+            }
+
+            if (find(a) == find(b)) {
+                bw.append("YES\n");
+            } else {
+                bw.append("NO\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    static int find(int node) {
+        while (node != parents[node]) {
+            node = parents[node];
+        }
+
+        return node;
+    }
+
+    static void merge(int a, int b) {
+        if (heights[a] < heights[b]) {
+            merge(b, a);
+            return;
+        }
+
+        parents[b] = a;
+
+        if (heights[a] == heights[b]) {
+            heights[a]++;
+        }
+    }
+}
