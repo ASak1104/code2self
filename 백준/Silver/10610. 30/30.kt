2630debd8956c7afff5446df89063cd14d05d84e
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-08-28 01:38:03 +0900

    [Silver IV] Title: 30, Time: 148 ms, Memory: 15732 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.kt
+++ 백준/Silver/10610. 30/30.kt
@@ -1,23 +1,34 @@
 fun main() {
     val br = System.`in`.bufferedReader()
-    val n = br.readLine().toMutableList()
+    val n = br.readLine()
 
     br.close()
 
+    val counts = IntArray(10)
     var tenCondition = false
     var sum = 0
 
     for (c in n) {
-        if (!tenCondition && c == '0') {
+        val num = c - '0'
+
+        if (!tenCondition && num == 0) {
             tenCondition = true
         }
 
-        sum += c - '0'
+        sum += num
+        counts[num]++
     }
 
     if (tenCondition && sum % 3 == 0) {
-        n.sortDescending()
-        println(n.joinToString(""))
+        val sb = StringBuilder()
+
+        for (num in 9 downTo 0) {
+            repeat(counts[num]) {
+                sb.append(num)
+            }
+        }
+
+        println(sb)
     } else {
         println(-1)
     }
