671e096b52a7a7422e0cb3c4c17ece4880cdbcd4
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-11-27 00:30:05 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1350.27 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.kt
@@ -0,0 +1,54 @@
+class Solution {
+
+    lateinit var deliveries: IntArray
+    lateinit var pickups: IntArray
+
+    var last = 0
+    var cap = 0
+    var dist = 0L
+    
+    fun solution(cap: Int, n: Int, deliveries: IntArray, pickups: IntArray): Long {
+        this.deliveries = deliveries
+        this.pickups = pickups
+        this.last = n - 1
+        this.cap = cap
+
+        while (last >= 0) {
+            simulate()
+        }
+
+        return dist
+    }
+
+    fun simulate() {
+        while (last >= 0 && deliveries[last] == 0 && pickups[last] == 0) {
+            last--
+        }
+
+        if (last < 0) {
+            return
+        }
+
+        drive(deliveries)
+        drive(pickups)
+    }
+
+    fun drive(array: IntArray) {
+        var count = cap
+        var house = last
+
+        while (house >= 0 && count > 0) {
+            if (count >= array[house]) {
+                count -= array[house]
+                array[house] = 0
+            } else {
+                array[house] -= count
+                count = 0
+            }
+
+            house--
+        }
+
+        dist += last + 1
+    }
+}
