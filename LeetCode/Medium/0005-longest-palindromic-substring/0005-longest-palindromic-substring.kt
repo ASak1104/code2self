2044c2cbdca6aaacd7892a12a067886df6c159a1
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-12-05 01:47:09 +0900

    Time: 205 ms (70.74%), Space: 37.6 MB (57.55%) - LeetHub

---
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.kt
@@ -0,0 +1,36 @@
+class Solution {
+    
+    var start = 0
+    var end = 0
+
+    fun longestPalindrome(s: String): String {
+        for (i in 0 until s.lastIndex) {
+            findPalindrome(s, i, i + 1)
+            findPalindrome(s, i - 1, i + 1)
+        }
+
+        return s.substring(start..end)
+    }
+
+    fun findPalindrome(s: String, leftStart: Int, rightStart: Int) {
+        var left = leftStart
+        var right = rightStart
+
+        while (left in s.indices && right in s.indices) {
+            if (s[left] != s[right]) {
+                break
+            }
+            
+            left--
+            right++
+        }
+        
+        left++
+        right--
+
+        if (end - start < right - left) {
+            start = left
+            end = right
+        }
+    }
+}
