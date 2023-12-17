cf2b0b696eb7d514560e0aae3898ed27fb376659
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-12-18 03:28:35 +0900

    [level 2] Title: 더 맵게, Time: 577.97 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,83 @@
+const heap = []
+
+const solution = (scoville, K) => {
+    let count = 0;
+
+    heapify(scoville);
+
+    while (scoville.length > 1 && scoville[0] < K) {
+        const first = heappop(scoville);
+        const second = heappop(scoville);
+
+        heappush(scoville, first + second * 2);
+        count++;
+    }
+
+    if (scoville[0] < K) {
+        return -1;
+    }
+
+    return count;
+}
+
+const heapify = (array) => {
+    for (let i = ~~(array.length / 2); i >= 0; i--) {
+        swapdown(array, i)
+    }
+}
+
+const heappush = (heap, value) => {
+    heap.push(value);
+
+    swapup(heap, heap.length - 1);
+}
+
+const heappop = (heap) => {
+    if (heap.length === 1) {
+        return heap.pop();
+    }
+
+    const pop = heap[0];
+
+    heap[0] = heap.pop();
+    swapdown(heap, 0);
+
+    return pop;
+}
+
+const swapdown = (heap, index) => {
+    const left = index * 2 + 1;
+    const right = index * 2 + 2;
+    let target = index;
+
+    if (left < heap.length && heap[left] < heap[target]) {
+        target = left;
+    }
+
+    if (right < heap.length && heap[right] < heap[target]) {
+        target = right;
+    }
+
+    if (index !== target) {
+        const temp = heap[index];
+
+        heap[index] = heap[target];
+        heap[target] = temp;
+
+        swapdown(heap, target);
+    }
+}
+
+const swapup = (heap, index) => {
+    let parent = ~~((index - 1) / 2);
+
+    while (index > 0 && heap[index] < heap[parent]) {
+        const temp = heap[index];
+
+        heap[index] = heap[parent];
+        heap[parent] = temp;
+
+        index = parent;
+        parent = ~~((index - 1) / 2);
+    }
+}
