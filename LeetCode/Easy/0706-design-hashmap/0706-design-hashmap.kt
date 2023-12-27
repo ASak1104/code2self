d66c5c585ed3cbfaf12775a1947ecf743efbaced
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2023-12-28 01:55:56 +0900

    Time: 406 ms (43.08%), Space: 59.9 MB (47.69%) - LeetHub

--- LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.kt
+++ LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.kt
@@ -1,19 +1,55 @@
-const val MAX_KEY = 1_000_000
-
 class MyHashMap() {
 
-    val values = IntArray(MAX_KEY + 1) { -1 }
+    val nodes: Array<Node> = Array(10_000) { Node(-1, -1) }
 
     fun put(key: Int, value: Int) {
-        values[key] = value
+        val hash: Int = hash(key)
+        var node: Node = nodes[hash]
+
+        while (node.key != key) {
+            if (node.next == null) {
+                node.next = Node(key, value)
+
+                return
+            }
+
+            node = node.next!!
+        }
+
+        node.value = value
     }
 
     fun get(key: Int): Int {
-        return values[key]
+        val hash: Int = hash(key)
+        var node: Node? = nodes[hash]
+
+        while (node != null && node.key != key) {
+            node = node.next
+        }
+
+        return node?.value ?: -1
     }
 
     fun remove(key: Int) {
-        values[key] = -1
+        val hash: Int = hash(key)
+        var node: Node = nodes[hash]
+
+        while (node.next != null) {
+            if (node.next!!.key == key) {
+                node.next = node.next?.next
+
+                return
+            }
+
+            node = node.next!!
+        }
+    }
+
+    fun hash(key: Int): Int = key % nodes.size
+
+    class Node(val key: Int, var value: Int) {
+
+        var next: Node? = null
     }
 
 }
