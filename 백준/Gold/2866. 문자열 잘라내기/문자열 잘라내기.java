7584a243652798570ef5d3de8aef7bb248acd3b6
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-01-04 11:08:52 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 532 ms, Memory: 39280 KB -BaekjoonHub

---
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static char[][] chars;
+    static int rows, cols;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        rows = Integer.parseInt(st.nextToken());
+        cols = Integer.parseInt(st.nextToken());
+
+        chars = new char[rows][cols];
+
+        for (int r = 0; r < rows; r++) {
+            chars[r] = br.readLine().toCharArray();
+        }
+
+        System.out.println(findBound());
+        br.close();
+    }
+
+    private static int findBound() {
+        int left = 1;
+        int right = rows - 1;
+        int bound = 0;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            boolean isDuplicate = isDuplicate(mid);
+
+            if (isDuplicate) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+                bound = mid;
+            }
+        }
+
+        return bound;
+    }
+
+    private static boolean isDuplicate(int start) {
+        Set<String> words = new HashSet<>();
+
+        for (int c = 0; c < cols; c++) {
+            StringBuilder sb = new StringBuilder(rows - start);
+
+            for (int r = start; r < rows; r++) {
+                sb.append(chars[r][c]);
+            }
+
+            String word = sb.toString();
+
+            if (words.contains(word)) {
+                return true;
+            }
+
+            words.add(word);
+        }
+
+        return false;
+    }
+
+}
