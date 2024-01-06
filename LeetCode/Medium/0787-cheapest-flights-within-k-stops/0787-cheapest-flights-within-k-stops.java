1152c4c9afce0220bbea3acb9962b08bf15c7564
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-01-07 08:43:22 +0900

    Time: 29 ms (7.26%), Space: 45.9 MB (5.51%) - LeetHub

---
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Solution {
+
+    static final int MAX = (int) 1e9;
+
+    static List<Edge>[] edges;
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        edges = new List[n];
+
+        for (int u = 0; u < n; u++) {
+            edges[u] = new ArrayList<>();
+        }
+
+        for (int[] flight : flights) {
+            edges[flight[0]].add(new Edge(flight[1], flight[2]));
+        }
+
+        return dijkstra(src, dst, k);
+    }
+
+    int dijkstra(int src, int dst, int k) {
+        int[][] dists = new int[edges.length][k + 1];
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
+
+        for (int[] dist : dists) {
+            Arrays.fill(dist, MAX);
+        }
+
+        dists[src][0] = 0;
+        pq.add(new Node(src, 0, 0));
+
+        while (!pq.isEmpty()) {
+            Node u = pq.poll();
+
+            for (Edge edge : edges[u.node]) {
+                if (u.cost + edge.weight >= dists[edge.node][u.count]) {
+                    continue;
+                }
+
+                dists[edge.node][u.count] = u.cost + edge.weight;
+
+                if (u.count < k) {
+                    pq.add(new Node(edge.node, u.count + 1, u.cost + edge.weight));
+                }
+            }
+        }
+
+        return Arrays.stream(dists[dst])
+                .filter(dist -> dist != MAX)
+                .min()
+                .orElse(-1);
+    }
+
+    static class Edge {
+
+        int node;
+        int weight;
+
+        public Edge(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+
+    }
+
+    static class Node {
+
+        int node;
+        int count;
+        int cost;
+
+        public Node(int node, int count, int cost) {
+            this.node = node;
+            this.count = count;
+            this.cost = cost;
+        }
+
+    }
+
+}
