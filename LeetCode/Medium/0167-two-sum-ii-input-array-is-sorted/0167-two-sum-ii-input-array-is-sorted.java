845af8f6e5d3b87362052b080f8fae945d4a691e
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-01-23 03:11:04 +0900

    Time: 1 ms (99.72%), Space: 46.3 MB (61.24%) - LeetHub

---
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public static final int NOT_FOUND = -1;
+
+    public static void main(String[] args) {
+        new Solution().twoSum(new int[]{2, 7, 11, 15}, 9);
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int prev = numbers[0] - 1;
+
+        for (int left = 0; left < numbers.length - 1; left++) {
+            if (numbers[left] == prev) {
+                continue;
+            }
+
+            int right = bisect(numbers, left + 1, target - numbers[left]);
+
+            if (right != NOT_FOUND) {
+                return new int[]{left + 1, right + 1};
+            }
+
+            prev = numbers[left];
+        }
+
+        return new int[0];
+    }
+
+    int bisect(int[] nums, int left, int target) {
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            int value = nums[mid];
+
+            if (value == target) {
+                return mid;
+            }
+
+            if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return NOT_FOUND;
+    }
+
+}
