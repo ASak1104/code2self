3bdc66930fa55ea64dcb4ac2fba6aed2b428beb6
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-01-23 19:01:50 +0900

    Time: 6 ms (47.99%), Space: 45.9 MB (77.34%) - LeetHub

--- LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,73 +1,35 @@
 class Solution {
 
-    int[][] matrix;
-    boolean found;
-
     public boolean searchMatrix(int[][] matrix, int target) {
-        this.matrix = matrix;
-
-        int colBound = searchRow(0, 0, matrix[0].length - 1, target);
-        int rowBound = searchCol(0, 0, Math.min(matrix.length - 1, colBound), target);
-
-        for (int r = 0; r <= rowBound; r++) {
-            for (int c = 0; c <= colBound; c++) {
-                if (found) {
-                    return true;
-                }
-
-                searchRow(r, 0, c, target);
+        for (int[] row : matrix) {
+            if (search(row, target)) {
+                return true;
             }
         }
 
-        return found;
+        return false;
     }
 
-    int searchRow(int row, int left, int right, int target) {
-        int bound = matrix[0].length - 1;
-
+    boolean search(int[] row, int target) {
+        int left = 0;
+        int right = row.length - 1;
+        
         while (left <= right) {
             int mid = (left + right) >>> 1;
-            int value = matrix[row][mid];
+            int value = row[mid];
 
             if (value == target) {
-                found = true;
-
-                return mid;
+                return true;
             }
 
             if (value < target) {
                 left = mid + 1;
             } else {
-                bound = mid;
                 right = mid - 1;
             }
         }
-
-        return bound;
-    }
-
-    int searchCol(int col, int left, int right, int target) {
-        int bound = matrix.length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) >>> 1;
-            int value = matrix[mid][col];
-
-            if (value == target) {
-                found = true;
-
-                return mid;
-            }
-
-            if (value < target) {
-                left = mid + 1;
-            } else {
-                bound = mid;
-                right = mid - 1;
-            }
-        }
-
-        return bound;
+        
+        return false;
     }
 
 }
