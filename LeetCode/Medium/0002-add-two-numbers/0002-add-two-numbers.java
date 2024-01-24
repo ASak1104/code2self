c40516fecd26f5075dc7b8d5b70be7d16e5ba9ea
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-01-25 02:25:52 +0900

    Time: 1 ms (100.00%), Space: 44.3 MB (38.37%) - LeetHub

---
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,43 @@
+import static java.util.Objects.nonNull;
+
+class Solution {
+
+    ListNode root = new ListNode();
+    ListNode node = root;
+    int offset = 0;
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        while (nonNull(l1) && nonNull(l2)) {
+            offset += l1.val + l2.val;
+            node.next = new ListNode(offset % 10);
+            offset /= 10;
+
+            node = node.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        offset = addSingleNode(l1);
+        offset = addSingleNode(l2);
+
+        if (offset > 0) {
+            node.next = new ListNode(offset);
+        }
+
+        return root.next;
+    }
+
+    int addSingleNode(ListNode target) {
+        while (nonNull(target)) {
+            offset += target.val;
+            node.next = new ListNode(offset % 10);
+            offset /= 10;
+
+            node = node.next;
+            target = target.next;
+        }
+
+        return offset;
+    }
+
+}
