f9a902c233baba5d145c802dbe5d2ece9ed5a666
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-02-06 01:47:27 +0900

    [Gold II] Title: 보석 도둑, Time: 2720 ms, Memory: 127976 KB

--- 백준/Gold/1202. 보석 도둑/보석 도둑 - 2.kt
+++ 백준/Gold/1202. 보석 도둑/보석 도둑 - 2.kt
@@ -1,33 +1,78 @@
 import java.util.*
+import kotlin.collections.ArrayDeque
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val (n, k) = StringTokenizer(readLine()).run { nextToken().toInt() to nextToken().toInt() }
-    val gems = MutableList(n) {
-        StringTokenizer(readLine()).run { nextToken().toInt() to nextToken().toInt() }
+val br = System.`in`.bufferedReader()
+
+var n = 0
+var k = 0
+
+lateinit var jewels: Array<Jewel>
+lateinit var bags: ArrayDeque<Int>
+
+fun main() {
+    with(StringTokenizer(br.readLine())) {
+        n = nextToken().toInt()
+        k = nextToken().toInt()
     }
-    gems.sortByDescending { it.second }
 
-    val bags = TreeMap<Int, Int>()
-    repeat(k) {
-        val bag = readLine().toInt()
-        bags[bag] = (bags[bag] ?: 0) + 1
+    jewels = Array(n) {
+        StringTokenizer(br.readLine()).run {
+            Jewel(nextToken().toInt(), nextToken().toInt())
+        }
     }
-    close()
 
-    var res = 0L
+    bags = ArrayDeque(k)
 
-    for (gem in gems) {
-        if (bags.lastKey() < gem.first) continue
+    repeat(k) { bags += br.readLine().toInt() }
 
-        res += gem.second
-        val (key, value) = bags.ceilingEntry(gem.first)
+    grandTheftAuto()
 
-        when (value) {
-            1 -> bags.remove(key)
-            else -> bags[key] = value - 1
+    br.close()
+}
+
+fun grandTheftAuto() {
+    var bigMoney = 0L
+
+    jewels.sortByDescending(Jewel::price)
+    bags.sort()
+
+    for (jewel in jewels) {
+        if (bags.isEmpty()) {
+            break
         }
 
-        if (bags.size == 0) break
+        val idx = findAtLeast(jewel.weight)
+
+        if (idx >= 0) {
+            bigMoney += jewel.price
+            bags.removeAt(idx)
+        }
     }
-    print(res)
+
+    println(bigMoney)
 }
+
+fun findAtLeast(target: Int): Int {
+    var left = 0
+    var right = bags.lastIndex
+    var atLeast = -1
+
+    while (left <= right) {
+        val mid = (left + right) ushr 1
+
+        if (bags[mid] == target) {
+            return mid
+        }
+
+        if (bags[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+            atLeast = mid
+        }
+    }
+
+    return atLeast
+}
+
+data class Jewel(val weight: Int, val price: Int)
