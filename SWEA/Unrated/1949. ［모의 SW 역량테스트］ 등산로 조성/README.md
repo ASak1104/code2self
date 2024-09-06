cfd1b268edbc4435de2a46b61dc33dd8ce138506
Hyeonwoo Kim <31983961+ASak1104@users.noreply.github.com>
2024-09-06 16:09:20 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 137 ms, Memory: 21,576 KB -BaekjoonHub

---
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 등산로 조성 - 1949 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PoOKKAPIDFAUq) 
+
+### 성능 요약
+
+메모리: 21,576 KB, 시간: 137 ms, 코드길이: 2,462 Bytes
+
+### 제출 일자
+
+2024-09-06 16:09
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    private static final int[][] weights = new int[][]{
+        new int[]{0, 1},
+        new int[]{1, 0},
+        new int[]{0, -1},
+        new int[]{-1, 0},
+    };
+
+    private static int[][] map;
+    private static int T, N, K, target, answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new int[N][N];
+            target = 0;
+            answer = 0;
+
+            for (int r = 0; r < N; r++) {
+                st = new StringTokenizer(br.readLine());
+
+                for (int c = 0; c < N; c++) {
+                    map[r][c] = Integer.parseInt(st.nextToken());
+                    target = Math.max(target, map[r][c]);
+                }
+            }
+
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    simulate(r, c, 1, true);
+
+                    map[r][c] -= K;
+                    simulate(r, c, 1, false);
+                    map[r][c] += K;
+                }
+            }
+
+            sb.append('#').append(t).append(' ').append(answer).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static void simulate(int ur, int uc, int hob, boolean cutable) {
+        if (map[ur][uc] == target) {
+            answer = Math.max(answer, hob);
+        }
+
+        for (int[] weight : weights) {
+            int vr = ur + weight[0];
+            int vc = uc + weight[1];
+
+            if (vr < 0 || vr >= N || vc < 0 || vc >= N) {
+                continue;
+            }
+
+            if (map[ur][uc] < map[vr][vc]) {
+                simulate(vr, vc, hob + 1, cutable);
+
+                if (cutable) {
+                    int origin = map[vr][vc];
+
+                    map[vr][vc] = Math.max(map[ur][uc] + 1, map[vr][vc] - K);
+                    simulate(vr, vc, hob + 1, false);
+                    map[vr][vc] = origin;
+                }
+            }
+        }
+    }
+
+}
