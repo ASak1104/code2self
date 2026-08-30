bb2230260094e12adc6d3ba3b066826453fd5023
codingmon <31983961+ASak1104@users.noreply.github.com>
2026-08-30 18:12:58 +0900

    Time: 73 ms (14.6%), Space: 52 MB (36.68%) - LeetHub

--- LeetCode/Medium/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.go
+++ LeetCode/Medium/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.go
@@ -1,21 +1,22 @@
 import (
-	"maps"
 	"math/rand/v2"
 )
 
 type RandomizedSet struct {
-	set map[int]struct{}
+	idxByVal map[int]int
+	vals     []int
 }
 
 func Constructor() RandomizedSet {
-	return RandomizedSet{set: make(map[int]struct{})}
+	return RandomizedSet{idxByVal: make(map[int]int), vals: make([]int, 0)}
 }
 
 func (s *RandomizedSet) Insert(val int) bool {
 	if s.has(val) {
 		return false
 	}
-	s.set[val] = struct{}{}
+	s.idxByVal[val] = len(s.vals)
+	s.vals = append(s.vals, val)
 	return true
 }
 
@@ -23,23 +24,24 @@ func (s *RandomizedSet) Remove(val int) bool {
 	if !s.has(val) {
 		return false
 	}
-	delete(s.set, val)
+	tdx := s.idxByVal[val]
+	ldx := len(s.vals) - 1
+
+	s.idxByVal[s.vals[ldx]] = tdx
+	s.vals[tdx], s.vals[ldx] = s.vals[ldx], s.vals[tdx]
+
+	delete(s.idxByVal, val)
+	s.vals = s.vals[:ldx]
+
 	return true
 }
 
 func (s *RandomizedSet) GetRandom() int {
-	idx := 0
-	target := rand.IntN(len(s.set))
-	for k := range maps.Keys(s.set) {
-		if idx == target {
-			return k
-		}
-		idx++
-	}
-	return 0
+	tdx := rand.IntN(len(s.vals))
+	return s.vals[tdx]
 }
 
 func (s *RandomizedSet) has(val int) bool {
-	_, ok := s.set[val]
+	_, ok := s.idxByVal[val]
 	return ok
 }
